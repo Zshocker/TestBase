a39269adfad19f0e8602f2717ef3ee25ee2c95ab
Zshocker <hicham.sentel-etu@etu.univh2c.ma>
2022-02-16 17:29:08 +0100

    sss

--- TestBase/Pages/Index.cshtml.cs
+++ TestBase/Pages/Index.cshtml.cs
@@ -14,19 +14,15 @@ namespace TestBase.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly DataContext dataContext;
 
-        public IndexModel(ILogger<IndexModel> logger,DataContext db)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            dataContext = db;
+            
         }
 
         public void OnGet()
         {
-            Etudiant etudiant = new Etudiant("Hicham", "R1333");
-            dataContext.etudiants.Add(etudiant);
-            dataContext.SaveChanges();
         }
     }
 }
